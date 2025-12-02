d7e902c9e68350298f499b6590aca629c2087f94
bhvalent <benhudvalentine@gmail.com>
2025-12-02 17:51:48 -0600

    Day 1 part 2

--- src/internal/app/day1/day1.go
+++ src/internal/app/day1/day1.go
@@ -2,6 +2,7 @@ package day1
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 
 	"github.com/bhvalent/adventOfCode2025/src/internal/utilities"
@@ -26,7 +27,6 @@ func HowManyTimesAtZero(filename string) int {
 			distance = distance * -1
 		}
 		currentPosition = getUpdatedPosition(currentPosition, distance)
-		fmt.Printf("Position: %d\n", currentPosition)
 		if currentPosition == 0 {
 			timesAtZero += 1
 		}
@@ -35,13 +35,64 @@ func HowManyTimesAtZero(filename string) int {
 	return timesAtZero
 }
 
+func HowManyTimesPassingZero(filename string) int {
+	lines, err := utilities.GetLinesFromFile(filename)
+	if err != nil {
+		fmt.Printf("err: %v", err)
+	}
+
+	// get instructions
+	instructions := getInstructions(lines)
+
+	// count times at zero
+	timesPassingZero := 0
+	startingPosition := 50
+	currentPosition := startingPosition
+	for _, i := range instructions {
+		distance := i.distance
+		if i.direction == "L" {
+			distance = distance * -1
+		}
+		updatedPosition, passes := getUpdatedPositionAndPasses(currentPosition, distance)
+		currentPosition = updatedPosition
+		timesPassingZero += passes
+	}
+
+	return timesPassingZero
+}
+
 func getUpdatedPosition(current int, distance int) int {
 	value := current + distance
 	value = value % 100
 	if value < 0 {
-		value = value + 100
+		value += 100
 	}
 	return value
+}
+
+func getUpdatedPositionAndPasses(current int, distance int) (int, int) {
+	value := current + distance
+
+	// calculate update value
+	newValue := value % 100
+	if newValue < 0 {
+		newValue += 100
+	}
+
+	passes := 0
+	// full rotations
+	if distance <= -100 || distance >= 100 {
+		passes += int(math.Abs(float64(distance))) / 100
+	}
+
+	// partial rotations
+	isRightPartialPass := distance > 0 && newValue < current
+	isLeftPartialPass := distance < 0 && (newValue > current && current != 0 || (newValue == 0 && current != 0))
+	if isRightPartialPass || isLeftPartialPass {
+		passes += 1
+	}
+
+	return newValue, passes
 }
 
 type Instruction struct {
--- src/internal/app/day1/day1_test.go
+++ src/internal/app/day1/day1_test.go
@@ -22,3 +22,27 @@ func TestHowManyTimesAtZero_ShouldReturnCorrectAnswer_WithRealData(t *testing.T)
 	}
 }
 
+func TestHowManyTimesPassingZero_ShouldReturnCorrectAnswer_WithTestData(t *testing.T) {
+	result := day1.HowManyTimesPassingZero("input_test_data.txt")
+	expected := 6
+	if result != expected {
+		t.Errorf("HowManyTimesPassingZero() = %d; Expected: %d", result, expected)
+	}
+}
+
+func TestHowManyTimesPassingZero_ShouldReturnCorrectAnswer_WithAdditionalTestData(t *testing.T) {
+	result := day1.HowManyTimesPassingZero("input_additional_test_data.txt")
+	expected := 31
+	if result != expected {
+		t.Errorf("HowManyTimesPassingZero() = %d; Expected: %d", result, expected)
+	}
+}
+
+func TestHowManyTimesPassingZero_ShouldReturnCorrectAnswer_WithRealData(t *testing.T) {
+	result := day1.HowManyTimesPassingZero("input_real_data.txt")
+	expected := 6819
+	if result != expected {
+		t.Errorf("HowManyTimesPassingZero() = %d; Expected: %d", result, expected)
+	}
+}
+
