272040a21e1125ad6317603310dc43aee5957694
bhvalent <benhudvalentine@gmail.com>
2025-12-03 16:43:53 -0600

    Day2 part 2

--- src/internal/app/day2/day2.go
+++ src/internal/app/day2/day2.go
@@ -8,7 +8,7 @@ import (
 	"github.com/bhvalent/adventOfCode2025/src/internal/utilities"
 )
 
-func GetInvalidRangeSum(filename string) int {
+func GetInvalidRangeSum(filename string, includeAnySequenceLength bool) int {
 	lines, err := utilities.GetLinesFromFile(filename)
 	if err != nil || len(lines) != 1 {
 		return 0
@@ -18,19 +18,19 @@ func GetInvalidRangeSum(filename string) int {
 
 	sum := 0
 	for _, r := range ranges {
-		sum += getRangeSum(r)
+		sum += getRangeSum(r, includeAnySequenceLength)
 	}
 
 	return sum
 }
 
-func getRangeSum(r *Range) int {
+func getRangeSum(r *Range, includeAnySequenceLength bool) int {
 	sum := 0
 	for i := r.start; i <= r.end; i++ {
-		if !isValidId(i) {
+		if (!includeAnySequenceLength && !isValidId(i)) || (includeAnySequenceLength && !isValidIdV2(i)) {
 			sum += i
 		}
-	}
+  }
 	return sum
 }
 
@@ -43,6 +43,42 @@ func isValidId(num int) bool {
 
 	mid := len(snum) / 2
 	return !(snum[:mid] == snum[mid:])
+}
+
+func isValidIdV2(num int) bool {
+	snum := strconv.Itoa(num)
+
+	mid := len(snum) / 2
+  for seqLen := mid; seqLen > 0; seqLen-- {
+    if len(snum) % seqLen != 0 {
+      continue
+    }
+
+    if hasRepeatingSequence(snum, seqLen) {
+      return false
+    }
+  }
+
+  return true
+}
+
+func hasRepeatingSequence(data string, seqLen int) bool {
+  seq := []string{}
+  for i := 0; i < len(data); i += seqLen {
+    if i + seqLen > len(data) {
+      continue
+    }
+
+    seq = append(seq, data[i:i+seqLen])
+  }
+
+  for i := 1; i < len(seq); i++ {
+    if seq[0] != seq[i] {
+      return false
+    } 
+  }
+
+  return true
 }
 
 type Range struct {
--- src/internal/app/day2/day2_test.go
+++ src/internal/app/day2/day2_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestGetInvalidRangeSum_ShouldReturnCorrectAnswer_WithTestData(t *testing.T) {
-	result := day2.GetInvalidRangeSum("input_test_data.txt")
+	result := day2.GetInvalidRangeSum("input_test_data.txt", false)
 	expected := 1227775554
 	if result != expected {
 		t.Errorf("GetInvalidRangeSum() = %d; Expected: %d", result, expected)
@@ -15,10 +15,26 @@ func TestGetInvalidRangeSum_ShouldReturnCorrectAnswer_WithTestData(t *testing.T)
 }
 
 func TestGetInvalidRangeSum_ShouldReturnCorrectAnswer_WithRealData(t *testing.T) {
-	result := day2.GetInvalidRangeSum("input_real_data.txt")
+	result := day2.GetInvalidRangeSum("input_real_data.txt", false)
 	expected := 40398804950
 	if result != expected {
 		t.Errorf("GetInvalidRangeSum() = %d; Expected: %d", result, expected)
+	}
+}
+
+func TestGetInvalidRangeSumV2_ShouldReturnCorrectAnswer_WithTestData(t *testing.T) {
+	result := day2.GetInvalidRangeSum("input_test_data.txt", true)
+	expected := 4174379265
+	if result != expected {
+		t.Errorf("GetInvalidRangeSumV2() = %d; Expected: %d", result, expected)
+	}
+}
+
+func TestGetInvalidRangeSumV2_ShouldReturnCorrectAnswer_WithRealData(t *testing.T) {
+	result := day2.GetInvalidRangeSum("input_real_data.txt", true)
+	expected := 65794984339
+	if result != expected {
+		t.Errorf("GetInvalidRangeSumV2() = %d; Expected: %d", result, expected)
 	}
 }
 
